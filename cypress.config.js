7c0fd5d34db7ddefce515b71fa2ed0cee314821c
Tales Henrique <tales.lima@ibm.com>
2024-09-13 11:54:05 -0300

    atualizando arquivos

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportHeight: 880,
+  viewportWidth: 1280,
+  projectId: '5a98y5',
+  e2e: {
+    setupNodeEvents(on, config) {},
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
