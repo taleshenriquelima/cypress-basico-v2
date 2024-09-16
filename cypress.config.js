8a667d0da16e923537f18a59a2ecdc06e163dc65
Tales Henrique <tales.lima@ibm.com>
2024-09-16 10:34:53 -0300

    commit cópia do prpjeto3

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,4 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  viewportHeight: 880,
-  viewportWidth: 1280,
-  projectId: '5a98y5',
-  e2e: {
-    setupNodeEvents(on, config) {},
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-  },
-})
+module.exports = {
+  "e2e": {},
+  "projectId": "5a98y5"
+}
